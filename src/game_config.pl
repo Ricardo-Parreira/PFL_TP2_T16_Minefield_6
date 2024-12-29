156e5ff004cddfab9b93b6eb6d2462112c3b9943
Proenca13 <146175170+Proenca13@users.noreply.github.com>
2024-12-29 13:36:08 +0000

    Update game_config.pl

--- src/game_config.pl
+++ src/game_config.pl
@@ -7,7 +7,7 @@ choose_game_type(Type) :-
     read_input([Ascii | _]),
     to_number(Ascii, Type),
     (   between(1, 4, Type)
-    ->  !
+    ->  game_type(Type),!
     ;   write('Invalid choice, please try again.'), nl,
         fail 
     ).
@@ -15,10 +15,16 @@ configure_game(Type, Config) :-
     write('Enter board size (e.g., 5 for a 5x5 board): '), nl,
     read_input([Ascii | _]),
     to_number(Ascii, BoardSize),
-    (   Type = 2 ; Type = 3 ; Type = 4
+    (   Type == 2 ; Type == 3 ; Type == 4
     ->  write('Enter difficulty level (1: Random, 2: Hard): '), nl,
+        repeat,
         read_input([Ascii | _]),
-        to_number(Ascii, Difficulty)
+        to_number(Ascii, Difficulty),
+        (   between(1, 2, Difficulty)
+        ->  !
+        ;   write('Invalid choice, please try again.'), nl,
+            fail 
+        )
     ;   Difficulty = none 
     ),
     Config = [Type,BoardSize,Difficulty].
@@ -26,5 +32,5 @@ configure_game(Type, Config) :-
 %GameState with Board, first player 
 initial_state(Config, GameState) :-
     create_board(empty, BoardSize, Board),
-    GameState = [Board,  Player1].
+    GameState = [Board,  'Player1'].
 
