63b3fda7827985819a7fbb96ee650fd2ea093b05
RicardoParreira <ricardo.parreira.tic@gmail.com>
2025-01-03 22:53:08 +0100

    connection_score working perfectly

--- src/greedy.pl
+++ src/greedy.pl
@@ -92,57 +92,59 @@ potential_score(Board, Colour, TotalScore) :-
 
 /* calculate the score based on the different paths it has, how big they are and if they move optimally in the right direction*/
 connection_score(Board, Colour, TotalScore) :-
-    length(Board, Size), 
-    findall(Score, (
-        nth1(Row, Board, RowList),      % For each row
-        nth1(Col, RowList, Colour),          % For each correct stone in the row
-        dfs_score(Board, Row-Col, Row-Col, Size, [], Colour, 0.0, ScoreRaw),
-        abs(ScoreRaw, Score)
-    ), AllScores),
+    length(Board, Size),
+    all_pieces(Board, Colour, AllPieces),
+    connection_score_calc(Board, Colour, Size, AllPieces, [], AllScores),
     list_sum(AllScores, TotalScore).
 
-dfs_score(_, _, Row-Col, _, Visited, _, CurrentScore, CurrentScore) :-
+connection_score_calc(_, _, _, [], _, []). % no more pieces to explore
+connection_score_calc(Board, Colour, Size, [Row-Col | Rest], Visited, [Score | Scores]) :-
+    \+ member(Row-Col, Visited),
+    dfs_score(Board, Row-Col, Row-Col, Size, [], VisitedAfterDFS, Colour, 0.0, ScoreRaw),
+    abs(ScoreRaw, Score),
+    append(Visited, VisitedAfterDFS, UpdatedVisited),
+    connection_score_calc(Board, Colour, Size, Rest, UpdatedVisited, Scores).
+connection_score_calc(Board, Colour, Size, [Row-Col | Rest], Visited, Scores) :-
+    member(Row-Col, Visited),
+    connection_score_calc(Board, Colour, Size, Rest, UpdatedVisited, Scores).
+
+dfs_score(_, _, Row-Col, _, Visited, Visited, _, CurrentScore, CurrentScore) :-
     member(Row-Col, Visited). % If already visited, return the current score.
 
 % visit the piece
-dfs_score(Board, OldRow-OldCol, Row-Col, Size, Visited, Colour, CurrentScore, FinalScore) :-
+dfs_score(Board, OldRow-OldCol, Row-Col, Size, Visited, SuperUpdatedVisited, Colour, CurrentScore, FinalScore) :- % UpdatedVisited is basically Visited but one step further and it is used to tell connection_score which pieces were visited
     \+ member(Row-Col, Visited),
-    write('dfs_score'),nl,
-    write(Row), write(' from row '), write(OldRow), nl, 
-    write('With score: '), write(CurrentScore), nl,
     neighbor_coords(Row, Col, Size, Neighbors),            % Get orthogonal neighbors
     score_calculator(OldRow, Row, CurrentScore, NewScore),
-    explore_neighbors_score(Board, Neighbors, Size, [Row-Col | Visited], Colour, Row-Col, NewScore, FinalScore).
+    UpdatedVisited = [Row - Col | Visited], %new Visited List
+    explore_neighbors_score(Board, Neighbors, Size, UpdatedVisited, SuperUpdatedVisited, Colour, Row-Col, NewScore, FinalScore).
 
 % Base case: No more neighbors to explore.
-explore_neighbors_score(_, [], _, _, _, _, AccumulatedScore, AccumulatedScore). 
-explore_neighbors_score(Board, [NextRow-NextCol | Rest], Size, Visited, Colour, Row-Col, CurrentScore, FinalScore) :-
+explore_neighbors_score(_, [], _, Visited, Visited, _, _, AccumulatedScore, AccumulatedScore). 
+explore_neighbors_score(Board, [NextRow-NextCol | Rest], Size, Visited, SuperUpdatedVisited, Colour, Row-Col, CurrentScore, FinalScore) :-
     \+ member(NextRow-NextCol, Visited),
     nth1(NextRow, Board, RowList),
     nth1(NextCol, RowList, Colour), % visit point only if it has a piece of correct colour
-    dfs_score(Board, Row-Col, NextRow-NextCol, Size, Visited, Colour, CurrentScore, NeighborScore), % Continue DFS
-    explore_neighbors_score(Board, Rest, Size, Visited, Colour, Row-Col, NeighborScore, FinalScore). % Process the rest of the neighbors of the piece at Row-Col
+    dfs_score(Board, Row-Col, NextRow-NextCol, Size, Visited, UpdatedVisited, Colour, CurrentScore, NeighborScore), % Continue DFS
+    explore_neighbors_score(Board, Rest, Size, UpdatedVisited, SuperUpdatedVisited, Colour, Row-Col, NeighborScore, FinalScore). % Process the rest of the neighbors of the piece at Row-Col
 
 %if a neighbour doesnt have the right stone in it continue
-explore_neighbors_score(Board, [NextRow-NextCol | Rest], Size, Visited, Colour, Row-Col, CurrentScore, FinalScore) :-
+explore_neighbors_score(Board, [NextRow-NextCol | Rest], Size, Visited, SuperUpdatedVisited, Colour, Row-Col, CurrentScore, FinalScore) :-
     nth1(NextRow, Board, RowList),
     \+ nth1(NextCol, RowList, Colour), % visit point only if it has a piece of correct colour
-    explore_neighbors_score(Board, Rest, Size, Visited, Colour, Row-Col, CurrentScore, FinalScore). % Process the rest of the neighbors of the piece at Row-Col
+    explore_neighbors_score(Board, Rest, Size, Visited, SuperUpdatedVisited, Colour, Row-Col, CurrentScore, FinalScore). % Process the rest of the neighbors of the piece at Row-Col
 
 %if a neighbor was already visited, skip it
-explore_neighbors_score(Board, [NextRow-NextCol | Rest], Size, Visited, Colour, Row-Col, CurrentScore, FinalScore) :-
+explore_neighbors_score(Board, [NextRow-NextCol | Rest], Size, Visited, SuperUpdatedVisited, Colour, Row-Col, CurrentScore, FinalScore) :-
     member(NextRow-NextCol, Visited),
-    explore_neighbors_score(Board, Rest, Size, Visited, Colour, Row-Col, CurrentScore, FinalScore). % Process the rest of the neighbors of the piece at Row-Col
+    explore_neighbors_score(Board, Rest, Size, Visited, SuperUpdatedVisited, Colour, Row-Col, CurrentScore, FinalScore). % Process the rest of the neighbors of the piece at Row-Col
 
 
 % add 1 when moving downwards
 score_calculator(OldRow, NextRow, OldScore, NewScore) :-
-    write('score_calculator'), nl,
-    write('calculating for '), write(OldRow-NextRow), nl,
     RowDiff is NextRow - OldRow,
     RowDiff =:= 1,
-    NewScore is OldScore + 100,
-    write('scores: '), write(OldScore-NewScore), nl.
+    NewScore is OldScore + 100.
 
 % add 1 when moving upwards
 score_calculator(OldRow, NextRow, OldScore, NewScore) :-
@@ -182,3 +184,12 @@ list_subset([], _). % Base case: empty list is always a subset
 list_subset([X|Rest], List2) :-
     select(X, List2, ReducedList2), % Remove one occurrence of X from List2
     list_subset(Rest, ReducedList2). % Recursively check the rest
+
+%get the coordinates to all of the colours pieces as Rou-Column
+all_pieces(Board, Colour, Pieces) :-
+    findall(
+        Row-Col,
+        (   nth1(Row, Board, RowList),
+            nth1(Col, RowList, Colour)),
+        Pieces
+    ).
